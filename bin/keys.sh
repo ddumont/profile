ee57473d38c7f2af891fb92f54074fd524524c59
Dan Dumont <ddumont@gmail.com>
2022-09-20 20:20:08 -0400

    add gpg key to keyring

--- bin/keys.sh
+++ bin/keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd $HOME > /dev/null
 pushd .ssh > /dev/null
-keychain --agents "ssh,gpg" id_rsa canofsleep ftrees
+keychain --agents "ssh,gpg" id_rsa canofsleep ftrees F24C2904AA1D0BDE
 popd > /dev/null
 source .keychain/$HOSTNAME-sh
 popd > /dev/null
